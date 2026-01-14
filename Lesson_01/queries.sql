50c06dcf04bbd2ca8798b26a5848b0af45edf65a
Anton Travkin <travkin.rh@gmail.com>
2026-01-14 23:58:53 +0300

    test

--- Lesson_01/queries.sql
+++ Lesson_01/queries.sql
@@ -1,4 +1,23 @@
-CREATE TABLE test (
+-- создаём базу данных для урока
+CREATE DATABASE lesson1;
+
+-- подключаемся к ней
+\c lesson1
+
+-- создаём таблицу
+CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    name TEXT
+    name VARCHAR(100),
+    age INT
 );
+
+-- проверяем таблицы
+\dt
+
+-- добавляем данные
+INSERT INTO students (name, age) VALUES
+('Anton', 23),
+('Maria', 22);
+
+-- смотрим данные
+SELECT * FROM students;
